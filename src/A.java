c93f2cfadfd42bab2d7337cbaf2d64684416f21d
Gaurav <gdharmale02@gmail.com>
2019-01-23 17:40:48 +0530

    This is Second

---
+++ src/A.java
@@ -0,0 +1,12 @@
+
+public class A {
+	public static void main(String[] args) {
+		int[][]x={{10,20,30,40},{50,60},{70,80}};
+		for(int []x1:x){
+			for(int x2:x1){
+				System.out.println(x2);
+			}
+		}
+	}
+
+}
---
+++ src/Pyramid2.java
@@ -0,0 +1,21 @@
+
+public class Pyramid2 {
+	public static void main(String[] args) {
+		
+		int[][]x={{10,20,30,40},{60,50}};
+		//int[]y={50,60,70,80};
+		for(int i=0;i<x.length;i++){
+			
+		for(int j=0;j<x[i].length;j++){
+			System.out.println(x[i][j]);
+		//for(int x2:x){
+			//System.out.println(x2);
+		//}
+		//for(int x3:y){
+			//System.out.print("Foe second Array");
+			//System.out.println(x3);
+		}
+	}
+	
+}
+}
